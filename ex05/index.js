897de7c4bf582dae9484bd9ef8b2fe4168502293
wb-wf210789 <wb-wf210789@IT-C02MF0GXF6T5.local>
2020-11-06 17:16:19 +0800

    lesson 05

--- ex05/index.js
+++ ex05/index.js
@@ -1,16 +1,17 @@
 const {EventEmitter} = require('events')
 module.exports = class Connection {
-    // ##BEGIN## 代码已加密
-gywgywgywgywgdqgdUgdvgdegdwgdmgd=gdqgdwgdUgdmgywgqRgqkgRd
-gywgywgywgywgywgywgywgywgdwgdkgdngdegqDgd9gRggd9gdvgdwgywgcRgywgdvgd9gRygywgcUgRggd9gdvgdwgcUgdDgdngdwgdwgd9gdmgqRgqkgc9gyw
-gywgywgywgywgRk
-
-gywgywgywgywgdqgdUgdvgdvgd9gdqgdwgdngdUgdvgqRgdDgdegdRgqkgywgRd
-gywgywgywgywgywgywgywgywgdwgdkgdngdegqDgd9gRggd9gdvgdwgqDgd9gdDgdngdwgqRgqdgdqgdUgdvgdvgqdgqlgdDgdegdRgqk
-gywgywgywgywgRk
-
-gywgywgywgywgdUgdvgcDgdUgdvgdvgqRgdqgdygqkgywgRd
-gywgywgywgywgywgywgywgywgdwgdkgdngdegqDgd9gRggd9gdvgdwgqDgdUgdvgqRgqdgdqgdUgdvgdvgqdgqlgdqgdygqk
-gywgywgywgywgRk
-    // ##END##
+    //暗号：冒泡排序
+    constructor() {
+        this.event = new EventEmitter();
+    }
+    onConn(fn) {
+        this.event.on('connection', fn); //订阅：执行回调
+        // on(event, listener)
+        // 为指定event事件注册一个监听器（入参：事件名，回调）
+    }
+    connection(arg) {
+        this.event.emit('connection', arg); //发布：监听传string
+        // emit(event, [arg1], [arg2], [...])
+        // 按监听器的顺序执行每个监听器，node打印arg
+    }
 }
