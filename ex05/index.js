bb3062a40092972e0b1cc9f336fe28acd3da07d4
wfanfan0520 <wangfan0520@126.com>
2020-11-06 22:02:09 +0800

    lesson 04

--- ex05/index.js
+++ ex05/index.js
@@ -6,8 +6,12 @@ module.exports = class Connection {
     }
     onConn(fn) {
         this.event.on('connection', fn); //订阅：执行回调
+        // on(event, listener)
+        // 为指定event事件注册一个监听器（入参：事件名，回调）
     }
     connection(arg) {
         this.event.emit('connection', arg); //发布：监听传string
+        // emit(event, [arg1], [arg2], [...])
+        // 按监听器的顺序执行每个监听器，node打印arg
     }
 }
