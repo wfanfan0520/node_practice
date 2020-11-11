b1f8739682598c665504f0e924414c39f5ee5080
wfanfan0520 <wangfan0520@126.com>
2020-11-11 21:46:23 +0800

    lesson 07

--- ex06/index.js
+++ ex06/index.js
@@ -1,17 +1,20 @@
 const crypto = require('crypto')
-const jsonToken = require("jsonwebtoken");
+// const jsonToken = require("jsonwebtoken");
 module.exports.createToken = token => {
     const ary = token.split('.')
     if (ary.length !== 3) {
         return
     }
     // 暗号：贪心算法
-    const secret = "12345678";
-    const options = { secret: "jwt_secret", key: "user" };
     return {
         getExp: () => {
-            let result = jsonToken.decode(token, secret, options);
-            return result["exp"];
+            // 【方式一】利用Buffer进行Base64解码
+            let str = new Buffer.from(ary[1],"base64").toString()
+            const result = JSON.parse(str)
+            return result.exp
+            // 【方式二】jsonToken.decode解码
+            // let result = jsonToken.decode(token, secret, options);
+            // return result["exp"];
         },
         verify: key => {
             const hmac = crypto.createHmac('SHA256', key).update(ary[0]+ '.' +  ary[1]).digest('base64');
